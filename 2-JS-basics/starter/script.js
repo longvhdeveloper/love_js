ef1bf0b65cddbccfb3006c4a3393340362866be7
vlong <vlong@yahoo-corp.jp>
2018-11-07 15:23:29 +0700

    Learn day 5

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -518,55 +518,193 @@ Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and heig
 GOOD LUCK
 */
 
-var john = {
-  fullName: "John Smith",
-  height: 1.7,
-  mass: 75,
-  calcBMI: function() {
-    return this.mass / (this.height * this.height);
+// var john = {
+//   fullName: "John Smith",
+//   height: 1.7,
+//   mass: 75,
+//   calcBMI: function() {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+//   }
+// };
+
+// var mark = {
+//   fullName: "Mark White",
+//   height: 1.72,
+//   mass: 77,
+//   calcBMI: function() {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+//   }
+// };
+
+// john.calcBMI();
+// mark.calcBMI();
+
+// if (john.bmi > mark.bmi) {
+//   console.log(
+//     john.fullName +
+//       "'s BMI is " +
+//       john.bmi +
+//       " greater than " +
+//       mark.fullName +
+//       "'s BMI is " +
+//       mark.bmi
+//   );
+// } else if (john.bmi < mark.bmi) {
+//   console.log(
+//     john.fullName +
+//       "'s BMI is " +
+//       john.bmi +
+//       " less than " +
+//       mark.fullName +
+//       "'s BMI is " +
+//       mark.bmi
+//   );
+// } else {
+//   console.log(
+//     john.fullName +
+//       "'s BMI is " +
+//       john.bmi +
+//       " same as " +
+//       mark.fullName +
+//       "'s BMI is " +
+//       mark.bmi
+//   );
+// }
+
+/*****************************
+ * Loops and iteration
+ */
+
+// for (var i = 1; i < 20; i += 2) {
+//   console.log(i);
+// }
+
+// // for loop
+// var john = ["John", "Smith", 1990, "designer", false];
+// for (var i = 0; i < john.length; i++) {
+//   console.log(john[i]);
+// }
+
+// // while loop
+// var i = 0;
+// while (i < john.length) {
+//   console.log(john[i]);
+//   i++;
+// }
+
+// continue and break statement
+// var john = ["John", "Smith", 1990, "designer", false];
+// for (var i = 0; i < john.length; i++) {
+//   if (typeof john[i] !== "string") {
+//     // continue;
+//     break;
+//   }
+//   console.log(john[i]);
+// }
+
+// Looping backwards
+// for (var i = john.length - 1; i > -1; i--) {
+//   console.log(john[i]);
+// }
+
+/*****************************
+ * CODING CHALLENGE 5
+ */
+
+/*
+Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
+
+This time, John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
+John likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+1. Create an object with an array for the bill values
+2. Add a method to calculate the tip
+3. This method should include a loop to iterate over all the paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+
+
+EXTRA AFTER FINISHING: Mark's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
+Mark likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is more than $300 (different than John).
+
+5. Implement the same functionality as before, this time using Mark's tipping rules
+6. Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by the number of elements in it (that's how you calculate the average)
+7. Calculate the average tip for each family
+8. Log to the console which family paid the highest tips on average
+
+GOOD LUCK
+*/
+var johnFamily = {
+  bills: [124, 48, 268, 180, 42],
+  tips: [],
+  amounts: [],
+  tipCalc: function() {
+    for (var i = 0; i < this.bills.length; i++) {
+      var percent;
+      if (this.bills[i] < 50) {
+        percent = 0.2;
+      } else if (this.bills[i] >= 50 && this.bills[i] <= 200) {
+        percent = 0.15;
+      } else {
+        percent = 0.1;
+      }
+      var tip = this.bills[i] * percent;
+      this.tips.push(tip);
+      this.amounts.push(tip + this.bills[i]);
+    }
   }
 };
 
-var mark = {
-  fullName: "Mark White",
-  height: 1.72,
-  mass: 77,
-  calcBMI: function() {
-    return this.mass / (this.height * this.height);
+var markFamily = {
+  bills: [77, 375, 110, 45],
+  tips: [],
+  amounts: [],
+  tipCalc: function() {
+    for (var i = 0; i < this.bills.length; i++) {
+      var percent;
+      if (this.bills[i] < 100) {
+        percent = 0.2;
+      } else if (this.bills[i] >= 100 && this.bills[i] <= 300) {
+        percent = 0.1;
+      } else {
+        percent = 0.25;
+      }
+      var tip = this.bills[i] * percent;
+      this.tips.push(tip);
+      this.amounts.push(tip + this.bills[i]);
+    }
   }
 };
 
-john.bmi = john.calcBMI();
-mark.bmi = mark.calcBMI();
+function calcAverageTips(tips) {
+  var sum = 0;
+  for (var i = 0; i < tips.length; i++) {
+    sum += tips[i];
+  }
+  return sum / tips.length;
+}
 
-if (john.bmi > mark.bmi) {
+johnFamily.tipCalc();
+markFamily.tipCalc();
+var johnAverageTips = calcAverageTips(johnFamily.tips);
+var markAverageTips = calcAverageTips(markFamily.tips);
+
+if (johnAverageTips > markAverageTips) {
   console.log(
-    john.fullName +
-      "'s BMI is " +
-      john.bmi +
-      " greater than " +
-      mark.fullName +
-      "'s BMI is " +
-      mark.bmi
+    "Average John's tips is " +
+      johnAverageTips +
+      " is greater than Average Mark's tips is " +
+      markAverageTips
   );
-} else if (john.bmi < mark.bmi) {
+} else if (johnAverageTips < markAverageTips) {
   console.log(
-    john.fullName +
-      "'s BMI is " +
-      john.bmi +
-      " less than " +
-      mark.fullName +
-      "'s BMI is " +
-      mark.bmi
+    "Average John's tips is " +
+      johnAverageTips +
+      " is less than Average Mark's tips is " +
+      markAverageTips
   );
 } else {
-  console.log(
-    john.fullName +
-      "'s BMI is " +
-      john.bmi +
-      " same as " +
-      mark.fullName +
-      "'s BMI is " +
-      mark.bmi
-  );
+  console.log("It is draw them");
 }
